7e94362e1cc0f865aeda7c254130ad16e0f10a1b
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-08-31 16:52:22 +0000

    adjust the files to suit the raspberry pi

---
+++ camera.py
@@ -0,0 +1,17 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.resolution=(100,100)
+def prev10():
+    camera.start_preview()
+    sleep(10)
+    camera.stop_preview()
+
+def capt():
+    camera.start_preview()
+    sleep(5)
+    camera.capture('image.jpg')
+    camera.stop_preview()
+
+capt()
--- postToServer.py
+++ postToServer.py
@@ -3,6 +3,8 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 import os
 import sched
 import time
+import camera
+
 
 s = sched.scheduler(time.time, time.sleep)
 delayUntilNextPost = 10  # seconds
@@ -11,8 +13,9 @@ priority = 1
 
 def postToServer():
     s.enter(delayUntilNextPost, priority, postToServer, ())
+    camera.capt()
     url = 'http://karenmcculloch.me/urbanfarming/data'
-    img = open(os.environ['HOME']+'/Desktop/img.jpg', 'rb')
+    img = open(os.environ['HOME']+'/piCode/image.jpg', 'rb')
     multipart_data = MultipartEncoder(
         fields={'image': ('img.jpg', img, 'image/*'),
                 'soilMoisture': '1',
